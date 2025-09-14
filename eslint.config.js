4e19a67aa5cabaf9fdda0a82d63bfd280176dfbe
IlyaVafin <rthrthrer5gh@gmail.com>
2025-09-14 14:10:01 +0300

    feat: #1 create eslint config + prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,64 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import reactRefreshPlugin from "eslint-plugin-react-refresh";
+import globals from "globals";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+
+  js.configs.recommended,
+
+
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2025,
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "react-refresh": reactRefreshPlugin,
+      prettier: prettierPlugin,
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      semi: "off",
+      quotes: [
+        "error",
+        "double",
+        { avoidEscape: true, allowTemplateLiterals: true },
+      ],
+      // Общие правила
+      "prefer-const": "error",
+      "prettier/prettier": ["error", { semi: false }],
+
+      // React правила
+      "react/jsx-uses-react": "off", // для React 17+ не нужен import React
+      "react/react-in-jsx-scope": "off",
+
+      // TypeScript правила
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
     },
   },
-])
+
+  // Игнорирование папок
+  {
+    ignores: ["node_modules", "dist"],
+  },
+];
